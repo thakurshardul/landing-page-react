21391191e549fdc728e4fa41f4e939bf0fd55d99
thakurshardul <thakurshardul1@gmail.com>
2024-08-13 02:16:40 +0530

    scroll effect and internal links implemented

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -2,7 +2,7 @@ import { testimonials } from "../constants";
 
 const Testimonials = () => {
   return (
-    <div className="mt-20 tracking-wide">
+    <div className="mt-20 tracking-wide" id="Testimonials">
       <h2 className="text-3xl sm:text-5xl lg:text-6xl text-center my-10 lg:my-20">
         What People are saying
       </h2>
--- src/components/Workflow.jsx
+++ src/components/Workflow.jsx
@@ -4,7 +4,7 @@ import { checklistItems } from "../constants";
 
 const Workflow = () => {
   return (
-    <div className="mt-20">
+    <div className="mt-20" id="Workflow">
       <h2 className="text-3xl sm:text-5xl lg:text-6xl text-center mt-6 tracking-wide">
         Accelerate your{" "}
         <span className="bg-gradient-to-r from-orange-500 to-orange-800 text-transparent bg-clip-text">
